012b572f06fa45b1f1d0fe2b089ff93b4ea53500
osneyrilxee <osneyrilxee@users.noreply.github.com>
2025-08-14 19:29:09 +0200

    updated add rust projet

---
+++ rust_interface_app/target/debug/deps/rust_interface_app-b208d14649828c00.d
@@ -0,0 +1,5 @@
+/home/rilxee/바탕화면/github_repo/travel agency/rust_interface_app/target/debug/deps/rust_interface_app-b208d14649828c00.d: src/main.rs
+
+/home/rilxee/바탕화면/github_repo/travel agency/rust_interface_app/target/debug/deps/rust_interface_app-b208d14649828c00: src/main.rs
+
+src/main.rs:
---
+++ rust_interface_app/target/debug/rust_interface_app.d
@@ -0,0 +1,1 @@
+/home/rilxee/바탕화면/github_repo/travel\ agency/rust_interface_app/target/debug/rust_interface_app: /home/rilxee/바탕화면/github_repo/travel\ agency/rust_interface_app/src/main.rs
